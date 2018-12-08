6ea3bbc732f664234a3f580c4c60178e82a5df7f
Terence Broadbent <44728186+BroadbentT@users.noreply.github.com>
2018-12-08 22:35:50 +0000

    Project File.
    Python script file to crack encrypted .pcap files captured via Wireshark.

--- pcap-cracker.py
+++ pcap-cracker.py
@@ -135,6 +135,7 @@ if os.path.isfile('./Fixerror.pcap') !=0:
 
 os.system("tcpdump -ennr " + filename + " '(type mgt subtype beacon)' | awk '{print $13}' | sed 's/[()]//g;s/......//' | sort | uniq > SSID.txt")
 print ""
+
 with open('SSID.txt', 'r') as myfile:
   ssid = myfile.read().replace('\n', '')
 os.remove('./SSID.txt')
